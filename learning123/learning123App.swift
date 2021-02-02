9f5e62d3816b9cd7eca7cca69ef928d77046ba74
Yüksel Toprak <yuekseltoprak@gmail.com>
2021-02-02 20:57:55 +0100

    Initial Commit

---
+++ learning123/learning123App.swift
@@ -0,0 +1,17 @@
+//
+//  learning123App.swift
+//  learning123
+//
+//  Created by Yüksel Toprak on 02.02.21.
+//
+
+import SwiftUI
+
+@main
+struct learning123App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
